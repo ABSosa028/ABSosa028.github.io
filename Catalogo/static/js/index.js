a82e00c2825c70040f726b9af058f8a68fcd3ae5
Allan Barillas <93857150+ABSosa028@users.noreply.github.com>
2023-06-28 12:19:26 -0600

    space
    add

--- Catalogo/static/js/index.js
+++
@@ -1,9 +0,0 @@
-
-//funcion pedir en whatsapp segun id enviando la imagen del id
-
-function pedir(id) {
-    const img = document.getElementById(id).src;
-    const url = `https://api.whatsapp.com/send?phone=+50233873562&text=Hola%2C%20deseo%20pedir%20el%20siguiente%20producto%20${img}`;
-    window.open(url);
-    }
-
